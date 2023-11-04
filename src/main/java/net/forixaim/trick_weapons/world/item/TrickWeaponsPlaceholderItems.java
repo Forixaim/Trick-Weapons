768f269f64587e2dec30d1ad32ed8191242d00e6
thebe <matthewcs@forixaim.net>
2023-11-04 16:24:35 -0700

    Added documentation for weapon animations, capabilities, and styles.

--- src/main/java/net/forixaim/trick_weapons/world/item/TrickWeaponsPlaceholderItems.java
+++ src/main/java/net/forixaim/trick_weapons/world/item/TrickWeaponsPlaceholderItems.java
@@ -10,8 +10,14 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import yesman.epicfight.world.item.DaggerItem;
 
+/**
+ * The registry of placeholder items
+ */
 public class TrickWeaponsPlaceholderItems {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TrickWeapons.MOD_ID);
+	/**
+	 * Adds a placeholder chakram item mainly to test.
+	 */
 	public static final RegistryObject<Item> wIron_Chakram = ITEMS.register("iron_chakram_placeholder", () -> new DaggerItem(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT), Tiers.IRON));
 
 	public static void register(IEventBus eventBus) {
