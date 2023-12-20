199bbd34e9e4e43e07c79187916b04b03c59fcdb
Matthew Chai School <Matthew Chai School@FORIXAIM>
2023-12-20 08:25:08 -0800

    Potential Fix of battle_style slot not appearing on the player.

--- src/main/java/net/forixaim/epic_fight_battle_styles/EpicFightBattleStyles.java
+++ src/main/java/net/forixaim/epic_fight_battle_styles/EpicFightBattleStyles.java
@@ -14,7 +14,6 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 
