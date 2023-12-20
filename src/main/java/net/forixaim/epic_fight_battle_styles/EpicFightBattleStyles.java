9de4167db0380db6b5163a1f9fe80f1feb291f41
Matthew Chai School <Matthew Chai School@FORIXAIM>
2023-12-20 08:24:32 -0800

    Potential Fix of battle_style slot not appearing on the player.

--- src/main/java/net/forixaim/epic_fight_battle_styles/EpicFightBattleStyles.java
+++ src/main/java/net/forixaim/epic_fight_battle_styles/EpicFightBattleStyles.java
@@ -1,24 +1,12 @@
 package net.forixaim.epic_fight_battle_styles;
 
-import com.mojang.logging.LogUtils;
-import net.forixaim.epic_fight_battle_styles.core_assets.skills.EpicFightBattleStyleCategories;
+
 import net.forixaim.epic_fight_battle_styles.core_assets.skills.EpicFightBattleStyleSkillSlots;
 import net.forixaim.epic_fight_battle_styles.initialization.registry.AnimationRegistry;
 import net.forixaim.epic_fight_battle_styles.initialization.registry.SkillRegistry;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -28,11 +16,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
-import yesman.epicfight.skill.SkillSlot;
+
 
 import static net.forixaim.epic_fight_battle_styles.initialization.registry.BlockRegistry.BLOCKS;
 import static net.forixaim.epic_fight_battle_styles.initialization.registry.CreativeTabRegistry.CREATIVE_MODE_TABS;
@@ -44,8 +28,6 @@ public class EpicFightBattleStyles {
 
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "epic_fight_battle_styles";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "epic_fight_battle_styles" namespace
     // Create a Deferred Register to hold Items which will all be registered under the "epic_fight_battle_styles" namespace
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
